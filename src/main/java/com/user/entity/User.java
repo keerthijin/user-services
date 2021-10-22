0889183b9afc55e8eeeb02e50e1bf8f0916a1350
Jaishal.Jamal <jaishal.jamal@ust.com>
2021-10-22 08:54:55 +0530

    Second Commit

--- src/main/java/com/user/entity/User.java
+++ src/main/java/com/user/entity/User.java
@@ -3,19 +3,22 @@ package com.user.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "user")
-
+@Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int id;
     @NotBlank(message = "Please enter the name")
--- src/main/java/com/user/repository/UserRepository.java
+++ src/main/java/com/user/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.user.repository;
 
 import com.user.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 
-public interface UserRepository extends MongoRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,Integer> {
     List<User> findByName(String name);
 }
